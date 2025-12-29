9e57270930510d4c4f801c40c6431f970aa541c9
antoinecollet5 <antoine.collet5@gmail.com>
2025-12-30 00:04:07 +0100

    DOCS: removing useless things about GoogleDocString

--- docs/sources/conf.py
+++ docs/sources/conf.py
@@ -18,16 +18,10 @@
 import datetime
 
 # FIX: https://github.com/mgaitan/sphinxcontrib-mermaid/issues/72
-import errno
 import os
 import sys
 
 import nested_grid_plotter
-import sphinx.util.osutil
-from sphinx.ext.napoleon.docstring import GoogleDocstring
-
-sphinx.util.osutil.ENOENT = errno.ENOENT
-
 
 package_path = os.path.abspath("..")
 sys.path.insert(0, package_path)
@@ -208,8 +202,7 @@ html_theme_options = {
         {
             "name": "Contributing",
             "url": (
-                "https://github.com/antoinecollet5/"
-                "nested_grid_plotter/CONTRIBUTING.rst"
+                "https://github.com/antoinecollet5/nested_grid_plotter/CONTRIBUTING.rst"
             ),
             "icon": "fa fa-gavel fa-fw",
         },
@@ -381,45 +374,3 @@ blockdiag_html_image_format = "SVG"
 
 # number figures allowing them to be referenced in the text
 numfig = True
-
-
-# Issue with attributes section, see:
-# https://github.com/sphinx-doc/sphinx/issues/2115
-# Solution: https://michaelgoerz.net/notes/extending-sphinx-napoleon-docstring-sections.html
-# -- Extensions to the  Napoleon GoogleDocstring class ---------------------
-
-# first, we define new methods for any new sections and add them to the class
-
-
-def parse_keys_section(self, section):
-    return self._format_fields("Keys", self._consume_fields())
-
-
-GoogleDocstring._parse_keys_section = parse_keys_section
-
-
-def parse_attributes_section(self, section):
-    return self._format_fields("Attributes", self._consume_fields())
-
-
-GoogleDocstring._parse_attributes_section = parse_attributes_section
-
-
-def parse_class_attributes_section(self, section):
-    return self._format_fields("Class Attributes", self._consume_fields())
-
-
-GoogleDocstring._parse_class_attributes_section = parse_class_attributes_section
-
-# we now patch the parse method to guarantee that the the above methods are
-# assigned to the _section dict
-
-
-def patched_parse(self):
-    self._sections["keys"] = self._parse_keys_section
-    self._sections["class attributes"] = self._parse_class_attributes_section
-    self._unpatched_parse()
-
-
-GoogleDocstring._unpatched_parse = GoogleDocstring._parse
-GoogleDocstring._parse = patched_parse
